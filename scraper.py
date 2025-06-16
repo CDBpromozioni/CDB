594d90fab4f33767f3340f40c52014017543b044
CDBpromozioni <pagamenti.cdb@gmail.com>
2025-06-16 16:14:55 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,17 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from db import get_db_connection
 
-def check_vinocom():
+def run_scraper():
     url = 'https://vino.com/'
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        # ATTENZIONE: qui è solo struttura base da personalizzare
-        products = soup.find_all('div')  # Da sostituire con il vero selettore
-        print("Pagina caricata correttamente, prodotti trovati:", len(products))
+        title = soup.title.string if soup.title else 'Titolo non trovato'
+        return f"Scraping riuscito! Titolo pagina: {title}"
     else:
-        print("Errore nel caricamento pagina:", response.status_code)
-
-if __name__ == '__main__':
-    check_vinocom()
+        return f"Errore nel caricamento: {response.status_code}"

97de9792b4c6a5b3832c8f47291014016a7e670e
CDBpromozioni <pagamenti.cdb@gmail.com>
2025-06-16 16:15:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from scraper import run_scraper
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Il monitoraggio è attivo."
+
+@app.route('/run-scraper')
+def run():
+    result = run_scraper()
+    return result
+
+if __name__ == '__main__':
+    app.run()

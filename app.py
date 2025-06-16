64916b80a45a2fcc0d81a82bd51efc75ea1a3966
CDBpromozioni <pagamenti.cdb@gmail.com>
2025-06-16 16:31:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,8 @@ def run():
     result = run_scraper()
     return result
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)

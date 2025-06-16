304ad5314da3315a2b6ce11578aba9b66303624c
CDBpromozioni <pagamenti.cdb@gmail.com>
2025-06-16 16:59:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from scraper import run_scraper
+from db import create_table
+import schedule
+import time
+import threading
+import os
 
 app = Flask(__name__)
 
@@ -8,12 +13,17 @@ def home():
     return "Il monitoraggio è attivo."
 
 @app.route('/run-scraper')
-def run():
-    result = run_scraper()
-    return result
+def manual_run():
+    return run_scraper()
 
-import os
+def schedule_scraping():
+    schedule.every().day.at("03:00").do(run_scraper)
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
 
 if __name__ == '__main__':
+    create_table()
+    threading.Thread(target=schedule_scraping).start()
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)

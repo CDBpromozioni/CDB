ec24161e2a4f8f67f06b30ac74ef9fafb4fffd15
CDBpromozioni <pagamenti.cdb@gmail.com>
2025-06-16 16:58:55 +0200

    Update db.py

--- db.py
+++ db.py
@@ -5,3 +5,20 @@ def get_db_connection():
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL)
     return conn
+
+def create_table():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS promotions (
+            id SERIAL PRIMARY KEY,
+            sito TEXT,
+            titolo TEXT,
+            url TEXT,
+            prezzo TEXT,
+            data TIMESTAMP DEFAULT NOW()
+        )
+    ''')
+    conn.commit()
+    cur.close()
+    conn.close()
